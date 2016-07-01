8018b5513f1660d22583e0bb4f64574c64ec8b68
James <jgstew101@gmail.com>
2016-07-01 17:04:06 -0500

    connection added

--- public/skills/CryptoTrader/index.js
+++ public/skills/CryptoTrader/index.js
@@ -1,4 +1,57 @@
 var alexa = require('alexa-app'),
 	app = new alexa.app('cryptoTrader'),
 	http = require("http"),
-	request = require("request");
+	request = require("request"),
+	autobahn = require('autobahn'),
+	wsuri = "wss://api.poloniex.com",
+	connection = new autobahn.Connection({
+		url: wsuri,
+		realm: "realm1"
+	});
+ 
+connection.onopen = function (session) {
+	function marketEvent (args,kwargs) {
+		console.log(args);
+	}
+	function tickerEvent (args,kwargs) {
+		console.log(args);
+	}
+	function trollboxEvent (args,kwargs) {
+		console.log(args);
+	}
+	//session.subscribe('BTC_XMR', marketEvent);
+	session.subscribe('ticker', tickerEvent);
+	//session.subscribe('trollbox', trollboxEvent);
+}
+connection.onclose = function () {
+  console.log("Websocket connection closed");
+}                      
+connection.open();
+
+app.launch(function(req, res) {
+	res.say("CryptoTrader ready for orders!").shouldEndSession(false);
+});
+
+app.intent('ticker', {
+	'slots': {'updateAll': 'LITERAL'},
+	'utterances': ['update all tickers']
+	},
+	function(req, res) {
+
+		var currency = '';
+		request
+			.get
+			('https://poloniex.com/public?command=returnTicker')
+			.on('data', function(data) {
+				currency += data;
+			})
+			.on('response', function(response) {
+				console.log('request status:' + response.statusCode);
+			})
+			.on('end', function() {
+				var market = JSON.parse(str);
+				console.log(market); 
+			})
+	});
+
+module.exports = app;
